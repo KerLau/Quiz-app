fdd0b3fa69f937fecf3ba0937d709f87899c2e95
Antonio_Macek <antoniomacek3@gmail.com>
2023-11-21 16:38:13 +0100

    code almost working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,11 @@
-import { useState } from "react";
-import Login from "./components/Login/Login.jsx";
-import Logout from "./components/Login/Logout/Logout.jsx";
-//import './Global.css';
+import Home from "./components/Home/Home.jsx";
+import React from "react";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = (username, password) => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
-    <div className="app">
-      <header className="app-header"></header>
-      <main className="app-main">
-        {isLoggedIn ? (
-          <>
-            <HomePage />
-            <Logout onLogout={handleLogout} />
-          </>
-        ) : (
-          <Login onLogin={handleLogin} />
-        )}
-      </main>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
