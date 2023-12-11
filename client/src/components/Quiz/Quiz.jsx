24d688d610ff3e9fb01151b988183726f2084977
Antonio_Macek <antoniomacek3@gmail.com>
2023-12-11 09:27:28 +0100

    component quiz done

--- client/src/components/Quiz/Quiz.jsx
+++ client/src/components/Quiz/Quiz.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import './Quiz.css';
+import React from "react";
+import "./Quiz.css";
 
 const Quiz = () => {
-   
-    const question = "Who is known as the father of computers?";
-    const answers = ["Charles Babbage", "Alan Turing", "John von Neumann", "Ada Lovelace"];
+  const question =
+    " o mokrmego moimoimger mpompmds mcfir nwi  nringn in90irng i9neinrgn inoierngoinm i 9inergm";
+  const answers = [
+    "Charles pdsihsdonb  pneobneb ev eljo;nvje rfbv ;ojnb eve",
+    "Alan Tuqopweuieqwrf i;ownovj we ojnwnjv km erknijrpn eqrv ;qeor;nv erveqorvne ring",
+    "John von Neumann",
+    "Ada Lovelace",
+  ];
 
-    return (
-        <div className="quiz-container">
-            <h1 className="quiz-header">QUIZ</h1>
-            <div className="question">{question}</div>
-            <div className="answers">
-                {answers.map((answer, index) => (
-                    <button key={index} className="answer-btn">{answer}</button>
-                ))}
-            </div>
-            <div className="quiz-navigation">
-                <button className="nav-btn">Back</button>
-                <button className="nav-btn">Next</button>
-                <button className="nav-btn">Cancel</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="quiz-container">
+      <div className="question">{question}</div>
+      <div className="answers">
+        {answers.map((answer, index) => (
+          <button key={index} className="answer-btn">
+            {answer}
+          </button>
+        ))}
+      </div>
+      <div className="quiz-navigation">
+        <button className="nav-btn">Back</button>
+        <button className="nav-btn">Cancel</button>
+        <button className="nav-btn">Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
